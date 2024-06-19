45e80bc3ede7db562d350a470c3af08c86fe1be7
MichalMiekina <michal.miekina98@gmail.com>
2024-06-19 21:36:02 +0200

    add asking for camera permission

--- app/src/main/java/com/example/drivertracking/ui/CameraPreview.kt
+++ app/src/main/java/com/example/drivertracking/ui/CameraPreview.kt
@@ -7,7 +7,11 @@ import androidx.camera.core.CameraSelector
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
@@ -15,14 +19,40 @@ import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberPermissionState
+import com.google.accompanist.permissions.PermissionStatus
+import com.google.accompanist.permissions.PermissionState
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun CameraPreview() {
     val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
+    val cameraPermissionState = rememberPermissionState(permission = Manifest.permission.CAMERA)
 
+    // Handle Camera Preview
+    CameraPreviewContent(
+        context = context,
+        lifecycleOwner = lifecycleOwner,
+        cameraPermissionState = cameraPermissionState
+    )
+
+    // Request Camera Permission if not granted
+    LaunchedEffect(Unit) {
+        if (cameraPermissionState.status != PermissionStatus.Granted) {
+            cameraPermissionState.launchPermissionRequest()
+        }
+    }
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun CameraPreviewContent(
+    context: Context,
+    lifecycleOwner: LifecycleOwner,
+    cameraPermissionState: PermissionState
+) {
     val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
     val previewView = remember { PreviewView(context) }
     var cameraExecutor: ExecutorService? by remember { mutableStateOf(null) }
@@ -32,25 +62,32 @@ fun CameraPreview() {
         onDispose { cameraExecutor?.shutdown() }
     }
 
-    LaunchedEffect(cameraProviderFuture) {
-        val cameraProvider = cameraProviderFuture.get()
-        val preview = Preview.Builder().build().also {
-            it.setSurfaceProvider(previewView.surfaceProvider)
+    if (cameraPermissionState.status == PermissionStatus.Granted) {
+        LaunchedEffect(cameraProviderFuture) {
+            val cameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder().build().also {
+                it.setSurfaceProvider(previewView.surfaceProvider)
+            }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    lifecycleOwner,
+                    cameraSelector,
+                    preview
+                )
+            } catch (exc: Exception) {
+                Log.e("CameraPreview", "Use case binding failed", exc)
+            }
         }
 
-        val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-        try {
-            cameraProvider.unbindAll()
-            cameraProvider.bindToLifecycle(
-                lifecycleOwner,
-                cameraSelector,
-                preview
-            )
-        } catch (exc: Exception) {
-            Log.e("CameraPreview", "Use case binding failed", exc)
+        AndroidView({ previewView }, modifier = Modifier.fillMaxSize())
+    } else {
+        // Show some UI to indicate the camera permission is not granted
+        Box(modifier = Modifier.fillMaxSize()) {
+            Text("Camera permission is required to use this feature")
         }
     }
-
-    AndroidView({ previewView })
 }
